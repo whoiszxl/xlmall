3182c8dbc87f337b689e915d8f2081a9a9f505b1
whoiszxl <whoiszxl@gmail.com>
2017-11-04 07:41:42 +0800

    update swagger2's setting.

--- xl-mall-api/src/main/java/com/zxlvoid/MallApplication.java
+++ xl-mall-api/src/main/java/com/zxlvoid/MallApplication.java
@@ -12,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @MapperScan("com.zxlvoid.dao")
-@EnableSwagger2
 public class MallApplication {
 	
 	public static void main(String[] args) {
---
+++ xl-mall-api/src/main/java/com/zxlvoid/controller/portal/ProductController.java
@@ -0,0 +1,13 @@
+package com.zxlvoid.controller.portal;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author whoiszxl
+ *
+ */
+@RestController
+public class ProductController {
+	
+}
